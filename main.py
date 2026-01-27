461be0886e6941e21a963c5639a5c5da25e2c0cb
zepox112 <zepoxkenny4@gmail.com>
2026-01-27 21:14:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
-# ... kodi nyingine ...
-api_key = os.getenv("API_KEY")
-bot_name = os.getenv("BOT_NAME")
-instruction = os.getenv("SYSTEM_MESSAGE")
-import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Zepox AI is Running!"
+
 if __name__ == "__main__":
+    # Sehemu ya kuwasha bot yako ya WhatsApp iwekwe hapa
+    
+    # Hii inatengeneza 'app' inayotakiwa na Render
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
