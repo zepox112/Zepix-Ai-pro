3ed38ceff1c44f48812f09a5557ad8c90adba5a7
zepox112 <zepoxkenny4@gmail.com>
2026-01-27 21:04:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import os
 api_key = os.getenv("API_KEY")
 bot_name = os.getenv("BOT_NAME")
 instruction = os.getenv("SYSTEM_MESSAGE")
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
