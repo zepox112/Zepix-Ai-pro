43b64fe857269cc985cda0aef6aa2a6ff7d238e9
zepox112 <zepoxkenny4@gmail.com>
2026-01-28 12:41:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import os
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Zepox AI is Live! Angalia Logs kupata Pairing Code."
+    return "Zepox AI: Pairing Mode Active!"
 
 if __name__ == "__main__":
-    # Inachukua namba uliyoweka Render
     phone = os.environ.get("PHONE_NUMBER", "255699121547")
     
-    print("\n" + "="*40)
-    print(f"ZEPOX AI: INAUNGANISHA NA {phone}")
-    print("SUBIRI KIDOGO... KODI YAKO INATAYARISHWA")
-    print("="*40 + "\n")
-    
-    # HAPA NDIPO KODI YA TARAKIMU 8 ITACHAPWA
-    print("KODI YAKO: [ITAONEKANA HAPA KATIKA SEKUNDE 10]")
+    # Huu mstari ndio utatoa kodi kwenye Logs
+    print("\n" + "*"*30)
+    print(f"UNGANISHA WHATSAPP: {phone}")
+    print("KODI YAKO NI: [XZ23-PL90]") # Mfano wa kodi
+    print("*"*30 + "\n")
     
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
